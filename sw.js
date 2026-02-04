c039cab74658eb56f8df660b80d8a02ea5affbe0
otaku-universe-news <117740844+otaku-universe-news@users.noreply.github.com>
2026-02-04 02:10:11 +0100

    Update and rename sw (6).js to sw.js

--- sw.js
+++ sw.js
@@ -3,4 +3,5 @@ const adkPushParams = {
 	channelId: 128, 
 	pubKey: 'BPg5N2jQ21bJcPsMf4-dC0DsznLBXnjsf71qb8oqF2g2bA4RH_527Em0SF1Dy-YBxR2B8wp1Tp4qKtZ8ujOwrw4' 
 }; 
+
 importScripts('//data.bvsrv.com/webpush/scripts/v1.1/sw.js');
